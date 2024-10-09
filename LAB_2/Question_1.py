47c37e882c79803e2b9d991d8a507aeeecc8c9de
stutid04 <dixitstuti2004@gmail.com>
2024-10-09 23:01:50 +0530

    Add files via upload

---
+++ LAB_2/Question_1.py
@@ -0,0 +1,6 @@
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+print("Distance between the points: ", distance)
