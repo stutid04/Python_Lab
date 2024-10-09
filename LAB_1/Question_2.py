2bbd866a77745a7ec7fb31692f975a8d8a82ac25
stutid04 <dixitstuti2004@gmail.com>
2024-10-09 20:47:23 +0530

    Add files via upload

---
+++ LAB_1/Question_2.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first floating point number: "))
+num2 = float(input("Enter the second floating point number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("Addition: {:.2f}".format(addition))
+print("Subtraction: {:.2f}".format(subtraction))
+print("Multiplication: {:.2f}".format(multiplication))
+print("Division: {:.2f}".format(division))
