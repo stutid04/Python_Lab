f4ddf1361f472f4e41e26ef53bb7267ffd9193b7
stutid04 <dixitstuti2004@gmail.com>
2024-10-09 20:49:10 +0530

    Add files via upload

---
+++ LAB_1/Question_6.py
@@ -0,0 +1,12 @@
+# This is a single-line comment
+# It explains that the following code prints "Hello, World"
+print("Hello, World!")  # This is an inline comment explaining what this line does
+'''
+This is a multi-line comment.
+You can write multiple lines of comments here.
+It's useful for providing detailed explanations or documentation.
+'''
+"""
+This is another example of a multi-line comment.
+You can use triple double quotes for the same purpose.
+"""
