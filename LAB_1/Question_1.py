88e0a5cb0cc066843e88a4d0bec4f5179983ff49
stutid04 <dixitstuti2004@gmail.com>
2024-10-09 20:43:43 +0530

    Add files via upload

---
+++ LAB_1/Question_1.py
@@ -0,0 +1,14 @@
+integer_value = 10
+float_value = 15.67
+string_value = "Python"
+boolean_value = True
+list_var=[1,2,3,4,5]
+set_var={4,5,6,7,8}
+tuple_var=(11,10,12,15)
+print("Integer Value:", integer_value, "Type:", type(integer_value))
+print("Float Value:", float_value, "Type:", type(float_value))
+print("String Value:", string_value, "Type:", type(string_value))
+print("Boolean Value:", boolean_value, "Type:", type(boolean_value))
+print("List Value:",list_var,"Type:",type(list_var))
+print("Set Value:",set_var,"Type:",type(set_var))
+print("Tuple Value:",tuple_var,"Type:",type(tuple_var))
