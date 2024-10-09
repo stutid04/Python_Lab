ec49722095e3d50477d035323b325bb406e8c690
stutid04 <dixitstuti2004@gmail.com>
2024-10-09 20:48:22 +0530

    Add files via upload

---
+++ LAB_1/Question_4.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+average = (num1 + num2) / 2
+deviation1 = num1 - average
+deviation2 = num2 - average
+print("Average:", average)
+print("Deviation of num1:", deviation1)
+print("Deviation of num2:", deviation2)
