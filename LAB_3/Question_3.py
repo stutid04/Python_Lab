bf3c842c1730bcbcf794640f78790e413cadda2f
stutid04 <dixitstuti2004@gmail.com>
2024-10-09 23:09:55 +0530

    Add files via upload

---
+++ LAB_3/Question_3.py
@@ -0,0 +1,12 @@
+percentage = float(input("Enter the percentage: "))
+if percentage >= 90:
+    grade = 'A'
+elif percentage >= 80:
+    grade = 'B'
+elif percentage >= 70:
+    grade = 'C'
+elif percentage >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+print("Grade: ", grade)
